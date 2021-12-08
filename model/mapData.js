1c066b9496600ec347d1f24b994eb4e4aa2d3d66
sunny_123 <sunny091194@gmail.com>
2021-12-08 16:05:27 +0530

    sunny
    Signed-off-by: sunny_123 <sunny091194@gmail.com>

---
+++ model/mapData.js
@@ -0,0 +1,47 @@
+export const markers = [
+    {
+      coordinate: {
+        latitude: 28.099812031751674,
+        longitude: 77.45176000000002,
+      },
+      title: "Charger place",
+      description: "Bike charger",
+      
+    },
+    {
+      coordinate: {
+        latitude: 28.096520031751674,
+        longitude: 77.91236000000002,
+      },
+      title: "Charger place ",
+      description: "AC-DC Charger",
+      
+    },
+    {
+      coordinate: {
+        latitude: 28.998650031751674,
+        longitude:77.08765300000002,
+      },
+      title: "Charger place",
+      description: "AC charger",
+      
+    },
+    {
+      coordinate: {
+        latitude: 28.931289031751674,
+        longitude: 77.09456000000002,
+      },
+      title: "Charger place",
+      description: "DC charger",
+      
+    },
+    {
+      coordinate: {
+        latitude: 28.938970031751674,
+        longitude: 77.0913456870000002,
+      },
+      title: "Charger place",
+      description: "AC001 charger",
+     
+    },
+];
